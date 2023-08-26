ca39caad9756e28f2928b2bce9bccc750a73785e
Natalia Maiatskaia <mia01kln@gmail.com>
2023-08-26 18:54:03 +0000

    fix new one main

--- thread/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from thread.models import Thread, Like, CommentLike, Comment
-
-admin.site.register(Thread)
-admin.site.register(Like)
-admin.site.register(CommentLike)
-admin.site.register(Comment)
--- user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import CustomUser, FollowingSystem
-
-
-admin.site.register(CustomUser)
-admin.site.register(FollowingSystem)
